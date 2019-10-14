01dc8bae6a4a482724ccd7d63d626790da27cde2
Janthó Dávid <janthod@inf.u-szeged.hu>
2019-10-14 02:12:20 +0200

    renamed the certification, and the public key

--- certif/Create_certificate.sh
+++ certif/Create_certificate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-openssl req -x509 -newkey rsa:4096 -keyout PublicKey.pem -out PrivateKey.pem -days 999 -nodes
+openssl req -x509 -newkey rsa:4096 -keyout Key.pem -out Certification.pem -days 999 -nodes
